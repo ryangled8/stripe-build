29f6d7c990d08c3acaa83fbb160b73f45c8dae94
Ryan Gledhill <ryan@engageinteractive.co.uk>
2020-08-25 14:10:03 +0100

    Replaces Blade layout with Vue layout

---
+++ resources/assets/js/components/global/Wrapper.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<site-header
+			v-bind="$props.siteHeader"
+		/>
+
+		<pre v-text="$props.siteFooter" />
+	</div>
+</template>
+
+<script>
+	import SiteHeader from './SiteHeader';
+
+	export default {
+		components: {
+			SiteHeader,
+		},
+
+		props: {
+			siteHeader: {
+				type: Object,
+				default: null,
+			},
+			siteFooter: {
+				type: Object,
+				default: null,
+			},
+		},
+	};
+</script>
