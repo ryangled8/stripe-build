f0ac899eca379b5015d3b9bd0c0751e150142421
Ryan Gledhill <ryan@engageinteractive.co.uk>
2020-09-02 16:10:53 +0100

    Adds Logo & Brand Stlyes

--- resources/views/layouts/partials/site-footer.blade.php
+++ resources/views/layouts/partials/site-footer.blade.php
@@ -1,62 +1,66 @@
-<footer class="relative z-site-footer">
-	<div class="e-container">
+<footer class="relative z-site-footer bg-brand-light-grey">
+	<div class="e-container py-20">
 		<div class="flex justify-between">
 			<div class="relative">
+				<a href="{{ $links['home'] }}">
+					<img class="w-20" src="/static/img/branding/logo-dark.png" alt="Stripe Logo">
+				</a>
+
 				@foreach ($site_footer['information']['items'] as $item)
 					<li class="list-none leading-normal">
-						<a class="no-underline">{{ $item['title'] }}</a>
+						<a class="text-sm no-underline">{{ $item['title'] }}</a>
 					</li>
 				@endforeach
 
 				@foreach ($site_footer['copyright']['items'] as $item)
 					<li class="absolute bottom-0 list-none leading-normal">
-						<a class="no-underline">{{ $item['title'] }}</a>
+						<a class="text-sm no-underline">{{ $item['title'] }}</a>
 					</li>
 				@endforeach
 			</div>
 
 			<div>
-				<p class="font-semibold">{{ $site_footer['products']['title'] }}</p>
+				<p class="mb-2 text-sm font-semibold">{{ $site_footer['products']['title'] }}</p>
 
 				@foreach ($site_footer['products']['items'] as $item)
 					<li class="list-none leading-normal">
-						<a class="no-underline">{{ $item['title'] }}</a>
+						<a class="text-sm no-underline hover:opacity-50">{{ $item['title'] }}</a>
 					</li>
 				@endforeach
 			</div>
 
 			<div>
-				<p class="font-semibold">{{ $site_footer['developers']['title'] }}</p>
+				<p class="mb-2 text-sm font-semibold">{{ $site_footer['developers']['title'] }}</p>
 
 				@foreach ($site_footer['developers']['items'] as $item)
 					<li class="list-none leading-normal">
-						<a class="no-underline">{{ $item['title'] }}</a>
+						<a class="text-sm no-underline hover:opacity-50">{{ $item['title'] }}</a>
 					</li>
 				@endforeach
 
-				<p class="font-semibold mt-8">{{ $site_footer['company']['title'] }}</p>
+				<p class="mb-2 text-sm font-semibold mt-8">{{ $site_footer['company']['title'] }}</p>
 
 				@foreach ($site_footer['company']['items'] as $item)
 					<li class="list-none leading-normal">
-						<a class="no-underline">{{ $item['title'] }}</a>
+						<a class="text-sm no-underline hover:opacity-50">{{ $item['title'] }}</a>
 					</li>
 				@endforeach
 			</div>
 
 			<div>
-				<p class="font-semibold">{{ $site_footer['use_cases']['title'] }}</p>
+				<p class="mb-2 text-sm font-semibold">{{ $site_footer['use_cases']['title'] }}</p>
 
 				@foreach ($site_footer['use_cases']['items'] as $item)
 					<li class="list-none leading-normal">
-						<a class="no-underline">{{ $item['title'] }}</a>
+						<a class="text-sm no-underline hover:opacity-50">{{ $item['title'] }}</a>
 					</li>
 				@endforeach
 
-				<p class="font-semibold mt-8">{{ $site_footer['resources']['title'] }}</p>
+				<p class="mb-2 text-sm font-semibold mt-8">{{ $site_footer['resources']['title'] }}</p>
 
 				@foreach ($site_footer['resources']['items'] as $item)
 					<li class="list-none leading-normal">
-						<a class="no-underline">{{ $item['title'] }}</a>
+						<a class="text-sm no-underline hover:opacity-50">{{ $item['title'] }}</a>
 					</li>
 				@endforeach
 			</div>
--- resources/views/layouts/partials/site-header.blade.php
+++ resources/views/layouts/partials/site-header.blade.php
@@ -1,14 +1,14 @@
 <header class="relative z-site-header">
 	<div class="e-container">
 		<div class="flex items-center justify-between mt-4">
-			<h1>
-				<a href="{{ $links['home'] }}">Stripe</a>
-			</h1>
+			<a href="{{ $links['home'] }}">
+				<img class="w-20" src="/static/img/branding/logo-dark.png" alt="Stripe Logo">
+			</a>
 	
 			<div class="flex">
 				@foreach ($site_header['nav'] as $item)
 					<li class="list-none mx-4">
-						<a class="no-underline" href="{{ $item['url'] }}">{{ $item['title'] }}</a>
+						<a class="font-bold no-underline hover:opacity-50" href="{{ $item['url'] }}">{{ $item['title'] }}</a>
 					</li>
 				@endforeach
 			</div>
