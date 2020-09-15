bfb3cbdcb01677c5c2838c03c7e97e781703a982
Ryan Gledhill <ryan@engageinteractive.co.uk>
2020-09-15 11:26:06 +0100

    Updates Footer Grid Layout

--- resources/views/layouts/partials/site-footer.blade.php
+++ resources/views/layouts/partials/site-footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="relative z-site-footer bg-brand-light-grey">
 	<div class="e-container py-20">
-		<div class="flex justify-between">
-			<div class="relative">
+		<div class="grid grid-cols-2 gap-8 md:grid-cols-4">
+			<div class="relative col-span-2 md:col-span-1">
 				<a href="{{ $links['home'] }}">
 					<img class="w-20" src="/static/img/branding/logo-dark.png" alt="Stripe Logo">
 				</a>
@@ -13,7 +13,7 @@
 				@endforeach
 
 				@foreach ($site_footer['copyright']['items'] as $item)
-					<li class="absolute bottom-0 list-none leading-normal">
+					<li class="pt-5 md:pt-0 md:absolute bottom-0 list-none leading-normal">
 						<a class="text-sm no-underline">{{ $item['title'] }}</a>
 					</li>
 				@endforeach
@@ -47,22 +47,26 @@
 				@endforeach
 			</div>
 
-			<div>
-				<p class="mb-2 text-sm font-semibold">{{ $site_footer['use_cases']['title'] }}</p>
+			<div class="grid gap-8 grid-cols-2 md:grid-cols-1 col-span-2 md:col-span-1">
+				<div>
+					<p class="mb-2 text-sm font-semibold">{{ $site_footer['use_cases']['title'] }}</p>
 
-				@foreach ($site_footer['use_cases']['items'] as $item)
-					<li class="list-none leading-normal">
-						<a class="text-sm no-underline hover:opacity-50">{{ $item['title'] }}</a>
-					</li>
-				@endforeach
+					@foreach ($site_footer['use_cases']['items'] as $item)
+						<li class="list-none leading-normal">
+							<a class="text-sm no-underline hover:opacity-50">{{ $item['title'] }}</a>
+						</li>
+					@endforeach
+				</div>
 
-				<p class="mb-2 text-sm font-semibold mt-8">{{ $site_footer['resources']['title'] }}</p>
+				<div>
+					<p class="mb-2 text-sm font-semibold mt-0">{{ $site_footer['resources']['title'] }}</p>
 
-				@foreach ($site_footer['resources']['items'] as $item)
-					<li class="list-none leading-normal">
-						<a class="text-sm no-underline hover:opacity-50">{{ $item['title'] }}</a>
-					</li>
-				@endforeach
+					@foreach ($site_footer['resources']['items'] as $item)
+						<li class="list-none leading-normal">
+							<a class="text-sm no-underline hover:opacity-50">{{ $item['title'] }}</a>
+						</li>
+					@endforeach
+				</div>
 			</div>
 		</div>
 	</div>
